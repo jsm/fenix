56eb38c9effdc5509141ca62da397a35cb432ed6
THORBEN ANDRESEN <thorben.andresen@gmail.com>
2018-04-03 17:11:21 -0400

    jsx inital setup

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import style from '../styles/styles.scss';
+import small from '../assets/small.jpg';
+
+const App = () => {
+  return (
+    <div className={style.panelDefault}>
+      <h1> Hello, world!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = ['lodash'];
+
+module.exports = {
+  entry: {
+    app: './src/index.jsx',
+    vendor: VENDOR_LIBS,
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.[name].[chunkhash].js',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+      },
+      //   {
+      //     test: /\.tsx?$/,
+      //     use: 'ts-loader',
+      //     exclude: /node_modules/,
+      //   },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 },
+          },
+          'image-webpack-loader',
+        ],
+      },
+      {
+        test: /\.(s)css$/,
+        // use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[local]___[hash:base64:5]',
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    // new webpack.DefinePlugin({
+    //   "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    // })
+  ],
+};
